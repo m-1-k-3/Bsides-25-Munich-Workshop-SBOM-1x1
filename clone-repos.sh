64df219052bae06881585a1e99f10e41cbbb1eeb
Marius Biebel <marius.biebel@outlook.de>
2025-11-14 19:15:14 +0000

    fixing repos

---
+++ clone-repos.sh
@@ -0,0 +1,9 @@
+git clone "https://github.com/keycloak/keycloak.git" \
+ && cd keycloak \
+ && git checkout "26.3.3" \
+ && cd ..
+
+git clone "https://github.com/apache/commons-io.git" \
+ && cd commons-io \
+ && git checkout "rel/commons-io-2.17.0" \
+ && cd ..
